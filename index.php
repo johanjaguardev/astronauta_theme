0ec5fcc8317ba751c416d4572b8cefefbaf5f895
Johan Vargas <pvargas@hugeinc.com>
2021-10-04 21:51:17 -0500

    add svg social icons, fix hero measures

--- index.php
+++ index.php
@@ -15,54 +15,27 @@
 get_header();
 ?>
 
-	<div class="hero">
+	<div class="hero hero-home">
+		<div class="container">
 
-		<?php
-		if ( have_posts() ) :
+		</div>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
 	</div>
 	<div class="noticias">
-
+		<div class="container"></div>
 	</div>
 	<div class="descripcion">
-
+		<div class="container"></div>
 	</div>
 	<div class="vision">
-
+		<div class="container"></div>
 	</div>
 	<div class="servicios">
-
+		<div class="container"></div>
 	</div>		
-	<div class="faq"></div>
+	<div class="faq">
+		<div class="container"></div>
+	</div>
 	
 <?php
 // get_sidebar();
--- notused.php
+++ notused.php
@@ -7,8 +7,44 @@ if ( $astronauta_description || is_customize_preview() ) :
 <?php endif; ?>
 
 <?php the_custom_logo();
-				if ( is_front_page() && is_home() ) :?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else :?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php endif;?>
+if ( is_front_page() && is_home() ) :?>
+<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+<?php else :?>
+<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+<?php endif;?>
+
+<div class="hero">
+
+<?php
+if ( have_posts() ) :
+
+	if ( is_home() && ! is_front_page() ) :
+		?>
+		<header>
+			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+		</header>
+		<?php
+	endif;
+
+	/* Start the Loop */
+	while ( have_posts() ) :
+		the_post();
+
+		/*
+		 * Include the Post-Type-specific template for the content.
+		 * If you want to override this in a child theme, then include a file
+		 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+		 */
+		get_template_part( 'template-parts/content', get_post_type() );
+
+	endwhile;
+
+	the_posts_navigation();
+
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+
+endif;
+?>
+</div>
