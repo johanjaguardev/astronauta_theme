95bd987367718c6503665575bce1edb7038dd650
Johan Vargas <pvargas@hugeinc.com>
2021-10-04 22:38:27 -0500

    hero css and coin image

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -1,6 +1,7 @@
 <div class="hero hero-home">
   <div class="container">
-
+    <h2>¿CÓMO ME CERTIFICO HALAL?</h2>
+    <a href="#">CONOCER MÁS</a>
+    <img class="hero__coin" src="<?php bloginfo('template_directory'); ?>/assets/coin.png" />
   </div>
-
 </div>
