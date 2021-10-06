160d7abb70cec9cbd1d76a28ce84acfe8dff6b97
Johan Vargas <pvargas@hugeinc.com>
2021-10-05 23:55:31 -0500

    add styles to description part

--- template-parts/content-descripcion.php
+++ template-parts/content-descripcion.php
@@ -1,5 +1,8 @@
 <div class="descripcion">
   <div class="container">
-    <pre>descripcion</pre>
+    <div class="descripcion__contenido">
+      <img src="<?php bloginfo('template_directory'); ?>/assets/desc-icon.png" alt="Descripcion Icono" />
+      <p>Respaldamos a las empresas de productos y servicios que atienden los mercados nacionales e internacionales de la Economía HALAL con la expedición del CERTIFICADO HALAL y la formación y capacitación del equipo de trabajo para desarrollar este segmento.</p>
+    </div>
   </div>
 </div>
