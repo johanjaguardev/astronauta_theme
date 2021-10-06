2071f5a23fc2f6fd26b580ad0c043aa6823d53a9
Johan Vargas <pvargas@hugeinc.com>
2021-10-06 03:00:34 -0500

    add faq image and css

--- template-parts/content-faq.php
+++ template-parts/content-faq.php
@@ -1,5 +1,14 @@
 <div class="faq">
 		<div class="container">
-			<pre>faq</pre>
+			<div class="faq__content">
+				<div class="faq__image">
+				</div>
+				<div class="faq__info">
+					<h3>PREGUNTAS FRECUENTES</h3>
+					<p>Resuelva sus dudas relacionadas con nuestros servicios</p>
+					<a href="#">CONOCER MÁS</a>
+				</div>
+			</div>
+			<div class="faq__bg"></div>
 		</div>
 	</div>
